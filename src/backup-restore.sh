1127572e26fc606ee0c50c89be5a7c0b30c07031
Your Name <you@example.com>
2023-03-14 18:41:14 +0100

    add encryption + restore + interfaces

--- src/backup-restore.sh
+++ src/backup-restore.sh
@@ -14,6 +14,17 @@ fi
 
 _hasFunctionOrFail "_backupRestore not Implemented by backup target [${BACKUP_TARGET}]" "_backupRestore"
 
+_info "Cleanup existing volumes"
+echo "Found volumes..."
+ls -1d /backup/*
+
+if ! yes_or_no "Do you want to delete content from existing volumes?"  
+then
+	echo "Volumes must be empty before restore."
+	exit 1
+fi
+
+_info "Cleanup existing volumes"
 if [ -S "$DOCKER_SOCK" ]; then
 	_containersThis="$(docker ps --filter status=running --filter name=docker-volume-backup --format '{{.ID}}')"	
 	_containersRunning="$(_dockerContainerFilter "status=running")"
@@ -31,20 +42,8 @@ if [ -S "$DOCKER_SOCK" ]; then
 	echo "$(echo "${_containersToStop}" | wc -w) containers to be stopped during restore"
 	_docker stop "${_containersToStop}"
 fi
-
-_info "Cleanup existing volumes"
-
-echo "Found volumes..."
-ls -1d /backup/*
-
-if ! yes_or_no "Do you want to delete content from existing volumes?"  
-then
-	echo "Volumes must be empty before restore."
-	_docker start "${_containersToStop}"
-	exit 1
-fi
-
 find ${_backupRestorTarget} -mindepth 2 -maxdepth 2 -print0 | xargs -0 -I {} rm -v -R {};
+
 _execFunctionOrFail "Restore backup ${_backupName}" "_backupRestore" "${_backupName} ${_backupRestorTarget}"
 _docker start "${_containersToStop}"
 
