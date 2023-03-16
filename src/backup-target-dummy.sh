ebc8edc3ffc7e75757c2ef2c8faa610fd7329ad9
Your Name <you@example.com>
2023-03-16 04:32:46 +0100

    add dummy target

---
+++ src/backup-target-dummy.sh
@@ -0,0 +1,64 @@
+
+function _backupTestConnection() {
+	echo "_backupTestConnection"		
+}
+
+function _backupPreUploadCommand() {
+	echo "_backupPreUploadCommand"
+}
+
+function _backupPostUploadCommand() {
+	echo "_backupPostUploadCommand"
+}
+
+function _backupArchiveOnTheFly() {
+	echo "_backupArchiveOnTheFly"
+}
+
+function _backupArchiveEncryptedOnTheFly() {
+	echo "_backupArchiveEncryptedOnTheFly"
+}
+
+function _backupIncremental() {
+	echo "_backupIncremental"
+}
+
+function _backupArchive() {
+	echo "_backupArchive"
+}
+
+function _backupArchiveEncrypted() {
+	echo "_backupArchiveEncrypted"
+}
+
+function _backupRemoveIncrementalPrevious() {
+	echo "_backupRemoveIncrementalPrevious"
+}
+
+function _backupRemoveArchiveOldest() {
+	echo "_backupRemoveArchiveOldest"		
+}
+
+function _backupRemoveArchiveOlderThanDays() {
+	echo "_backupRemoveArchiveOlderThanDays"
+}
+
+function _backupRestoreListFiles() {
+	echo "_backupRestoreListFiles"
+}
+
+function _backupRestore() {
+	echo "_backupRestore"
+}
+
+function _backupImagesEncryptedOnTheFly() {
+	echo "_backupImagesEncryptedOnTheFly"
+}
+
+function _backupImagesOnTheFly() {
+	echo "_backupImagesOnTheFly"
+}
+
+function _backupRemoveImages() {
+	echo "_backupRemoveImages"
+}
