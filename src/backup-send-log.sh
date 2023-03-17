982f0715aad9f106d526287df98e6d7bfb3613a7
Your Name <you@example.com>
2023-03-17 21:32:19 +0100

    finalize notifications

--- src/backup-send-log.sh
+++ src/backup-send-log.sh
@@ -19,8 +19,4 @@ if [[ ! -e "${_backupLog}" ]]; then
 	exit 1
 fi
 
-if docker run -t --rm containrrr/shoutrrr send --url "${BACKUP_NOTIFICATION_URL}" --message "$(cat "${_backupLog}" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")";
-	then echo "Successfull"
-	else echo "Failed"
-fi
-
+docker run -t --rm containrrr/shoutrrr send --url "${BACKUP_NOTIFICATION_URL}" --message "$(cat "${_backupLog}" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")"
